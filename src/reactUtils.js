8c4cad2b23077756064de6784f34f6d9d7062280
phola <info@phola.com>
2016-09-09 20:55:55 +0100

    fix null child and prod build

--- src/reactUtils.js
+++ src/reactUtils.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 export const deepFilterByComponentType = (children, type, results = []) => {
   React.Children.forEach(children, child => {
-    if (child.type === type) {
-      results.push(child)
-    }
-    if (child && child.props && child.props.children) {
-      deepFilterByComponentType(child.props.children, type, results)
+    if (child) {
+      if (child.type === type) {
+        results.push(child)
+      }
+      if (child.props && child.props.children) {
+        deepFilterByComponentType(child.props.children, type, results)
+      }
     }
   })
   return results
@@ -14,11 +16,13 @@ export const deepFilterByComponentType = (children, type, results = []) => {
 
 export const deepFilterByType = (children, type, results = []) => {
   React.Children.forEach(children, child => {
-    if (typeof child === type) {
-      results.push(child)
-    }
-    if (child && child.props && child.props.children) {
-      deepFilterByType(child.props.children, type, results)
+    if (child) {
+      if (typeof child === type) {
+        results.push(child)
+      }
+      if (child.props && child.props.children) {
+        deepFilterByType(child.props.children, type, results)
+      }
     }
   })
   return results
