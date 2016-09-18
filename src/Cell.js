0df4c267ff496cffb694f323f3fb266134c15ef2
phola <info@phola.com>
2016-09-18 08:36:24 +0100

    null check for context

--- src/Cell.js
+++ src/Cell.js
@@ -4,19 +4,19 @@ import XLSX from 'xlsx-style'
 
 export class Cell extends Component {
 
-  componentDidMount() {
-    if (this.context.getCell) this.context.getCell(this.mapCell(this.props))
+  componentDidMount () {
+    if (this.context && this.context.getCell) this.context.getCell(this.mapCell(this.props))
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate (nextProps, nextState) {
     var updated = shallowCompare(this, nextProps, nextState)
     if (updated) {
-      if (this.context.getCell) this.context.getCell(this.mapCell(nextProps))
+      if (this.context && this.context.getCell) this.context.getCell(this.mapCell(nextProps))
     }
     return updated
   }
 
-  mapCell(props) {
+  mapCell (props) {
     var { cellRef, col, row, colSpan, rowSpan, children = '', cellStyle = {}, type = 's', formula } = props
     var { sheet = 'Sheet1' } = this.context
     var cell_ref, merge
@@ -35,21 +35,21 @@ export class Cell extends Component {
     }
 
     var mapped = {
-        t: type,
-        s: cellStyle
-        // c: [{a: 'comment.author', t: 'comment.t', r: 'comment.r'}]
-      }
-      // formulas not supported at the mo
-      if (formula) {
-        mapped.f = children
-      }else {
-        mapped.v = children
-      }
+      t: type,
+      s: cellStyle
+    // c: [{a: 'comment.author', t: 'comment.t', r: 'comment.r'}]
+    }
+    // formulas not supported at the mo
+    if (formula) {
+      mapped.f = children
+    }else {
+      mapped.v = children
+    }
 
-      return { cellRef: cell_ref, data: mapped, merge: merge, col: col, row: row, sheet: sheet }
+    return { cellRef: cell_ref, data: mapped, merge: merge, col: col, row: row, sheet: sheet }
   }
 
-  render() {
+  render () {
     if (this.context.renderCells) {
       const { children } = this.props
       if (typeof children === 'string') return <span>{children}</span>
@@ -66,10 +66,10 @@ Cell.propTypes = {
   cellRef: React.PropTypes.string,
   children: React.PropTypes.string,
   cellStyle: React.PropTypes.object
-};
+}
 
 Cell.contextTypes = {
   getCell: React.PropTypes.func,
-  sheet:  React.PropTypes.string,
+  sheet: React.PropTypes.string,
   renderCells: React.PropTypes.bool
-};
+}
